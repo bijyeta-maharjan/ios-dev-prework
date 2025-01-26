a282554a69c4d64d6745858bdbffbc05ce09a284
bijyetamaharjan <mahar25b@mtholyoke.edu>
2025-01-26 14:50:30 -0500

    Initial Commit

---
+++ wise.ai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wise.ai
+//
+//  Created by Bijyeta Maharjan on 1/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
